e081172dfb3a1af61faff08cf65a577e6f62cdca
sami <dcil142@samiti.speedport.ip>
2021-02-22 00:56:05 +0100

    first commit

---
+++ js/Config.js
@@ -0,0 +1,9 @@
+export default class Config {
+  static FINN_HUB_TOKEN = "btgsi4f48v6u6od76120";
+  static IS_CACHE_STOCK_ENABLED = false;
+  static IS_MOCK_STOCK_ENABLED = false;
+
+  static ALPHA_VANtAGE_TOKEN = "KS83EIH10ASG1URC";
+  static IS_CACHE_SEARCH_ENABLED = false;
+  static IS_MOCK_SEARCH_ENABLED = false;
+}
